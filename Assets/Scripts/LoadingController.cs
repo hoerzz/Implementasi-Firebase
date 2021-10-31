6010fea0132445abeae65d410fdf688338f63a75
hoerzz <rudinhoer160@gmail.com>
2021-10-31 17:56:57 +0700

    first commit

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+ 
+
+public class LoadingController : MonoBehaviour
+{
+    private void Start ()
+    {
+
+        UserDataManager.Load ();
+
+        SceneManager.LoadScene (1);
+
+    }
+
+}
